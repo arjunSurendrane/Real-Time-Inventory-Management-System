cdec9f759c94b794d497eba096ecb30405ea576f
Arjun E <arjunrk907@gmail.com>
2023-03-03 01:54:13 +0530

    create rout
    create route and  add crud operations for business

--- Server/routes/inventory.js
+++ Server/routes/inventory.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { retrieveInventory } from '../controller/inventory.js';
+import { createInventory, deleteInventory, retrieveInventory, updateInventory } from '../controller/inventory.js';
 const router = express.Router()
 
 
 router.get('/', retrieveInventory)
+router.get('/:id', retrieveInventory)
+router.post('/', createInventory)
+router.put('/:id', updateInventory)
+router.delete('/:id', deleteInventory)
 
 
 export default router
