3a148409d0d058af4ce7d31b8b31dd9238ff566d
Arjun E <arjunrk907@gmail.com>
2023-03-03 00:59:55 +0530

    Connected to db
     server connected to mongoodb using mongoose

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import inventoryRouter from './routes/inventory.js'
+import { connectToDB } from './utils/db.js';
 dotenv.config({ path: './config.env' })
 const app = express()
 
 
+// connect to db
+connectToDB()
+
+// router
 app.use('/inventory', inventoryRouter)
 
-
+// create server and connected to localhost
 app.listen(process.env.PORT, () => {
     console.log(`Connected to localhost :${process.env.PORT}`)
 })
