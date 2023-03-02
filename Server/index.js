b7f18b2101f790c28c24d68e7e94c315fee1f0b9
Arjun E <arjunrk907@gmail.com>
2023-03-03 00:32:04 +0530

    fix bugs

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import inventoryRouter from './routes/inventory.js'
 dotenv.config({ path: './config.env' })
 const app = express()
 
+
+app.use('/inventory', inventoryRouter)
 
 
 app.listen(process.env.PORT, () => {
