10f26a5becefef5dbc17c933ab95418a413ac1bc
Arjun E <arjunrk907@gmail.com>
2023-03-03 12:30:20 +0530

    complete crud operations

--- Server/controller/inventory.js
+++ Server/controller/inventory.js
@@ -1,7 +1,14 @@
 import { create, getAllInventory, getById, remove, update } from "../services/inventory.js"
+import catchAsync from "../utils/catchAsync.js";
+import AppError from "../utils/AppError.js";
 
 
-
+/**
+ * Response function
+ * @param {Object} res - response
+ * @param {String} status - response status
+ * @param {Object} data - response data
+ */
 const response = (res, status, data) => {
     res.status(status).json({
         status: 'success',
@@ -9,34 +16,58 @@ const response = (res, status, data) => {
     })
 }
 
-export const retrieveInventory = async (req, res) => {
+/**
+ * Retrieve all product details from inventory collection
+ * @param {Object} req - request data
+ * @param {Object} res - response
+ */
+export const retrieveInventory = catchAsync(async (req, res, next) => {
     const data = await getAllInventory();
     response(res, 200, data)
-}
+})
 
-
-export const inventory = async (req, res) => {
+/**
+ * Retrieve a specific product details from inventory collection
+ * @param {Object} req - request data
+ * @param {Object} res - response
+ */
+export const inventory = catchAsync(async (req, res, next) => {
     const { id } = req.params
     const data = await getById(id)
+    if (!data) return next(new AppError('This product not exist in inventory collection', 404))
     response(res, 200, data)
-}
+})
 
-export const createInventory = async (req, res) => {
+/**
+ * Add a new product details into inventory collection
+ * @param {Object} req 
+ * @param {Object} res 
+ */
+export const createInventory = catchAsync(async (req, res, next) => {
     const { Name, Description, Quantity, Price, supplierId } = req.body
     const data = await create({ Name, Description, Quantity, Price, supplierId })
     response(res, 200, data)
-}
+})
 
-export const updateInventory = async (req, res) => {
+/**
+ * Update Product details 
+ * @param {Object} req 
+ * @param {Object} res 
+ */
+export const updateInventory = catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const updateData = req.body;
     const data = await update(id, updateData)
     response(res, 200, data)
-}
+})
 
-
-export const deleteInventory = async (req, res) => {
+/**
+ * Delete product data from inventory collection
+ * @param {Object} req 
+ * @param {Object} res 
+ */
+export const deleteInventory = catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const data = await remove(id)
     response(res, 200, data)
-}
+}) 
