be3e93362db612e0da2bb27e83d4e1f8bb75d0d3
Arjun E <arjunrk907@gmail.com>
2023-03-03 22:34:25 +0530

    enhance the socket IO integration in the vite react app.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,11 @@
+import { useEffect, useState } from "react";
+import { createWebSocket } from "./api/socket";
+
 function App() {
+  useEffect(() => {
+    createWebSocket();
+  }, []);
+  const [response, setResponse] = useState("");
   return <div>App</div>;
 }
 
