8ce5b86395d5f8698cc2fdd06327ca3631ffd71a
Arjun E <arjunrk907@gmail.com>
2023-03-03 12:34:27 +0530

    create config
    use a seperate file for connect to db

---
+++ Server/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+/**
+ * Connect to Mongodb
+ * @description - this function is used to connect express server with moongodb using mongoose
+ */
+export const connectToDB = async () => {
+    try {
+        const db = process.env.MONGODB_KEY
+        await mongoose.connect(db)
+        console.log('connected to mongodb')
+    } catch (error) {
+        console.log(`error : ${error}`)
+    }
+
+}
